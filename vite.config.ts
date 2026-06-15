aedb77034e5ae72cd9482a65fd806f80a68fc526
Wonbeen Lee <1been.lee@gmail.com>
2026-06-15 23:05:33 +0900

    feat & style: support multi-language HTML entries on Vercel and upgrade personal favicon logo

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: "es2022"
+    target: "es2022",
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        en: "index.en.html"
+      }
+    }
   }
 });
